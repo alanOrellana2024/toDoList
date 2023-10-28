551f857ff84f2da7a3c6139317311ce5aa3ce67c
Alan Orellana <alanantonioorellanaarguera@gmail.com>
2023-10-27 21:40:05 -0600

    :sparkles:new task list class and variable creation

--- src/mainpage/main.js
+++ src/mainpage/main.js
@@ -14,3 +14,25 @@ class Tarea {
     }
 }
 
+// Clase Lista de Tareas
+class ListaTareas {
+    constructor(nombre) {
+        this.nombre = nombre;
+        this.tareas = [];
+    }
+
+    agregarTarea(texto) {
+        const nuevaTarea = new Tarea(texto);
+        this.tareas.push(nuevaTarea);
+    }
+}
+
+// Variables
+const taskInput = document.getElementById('taskInput');
+const addTaskButton = document.getElementById('addTask');
+const taskList = document.getElementById('taskList');
+const taskListSelect = document.getElementById('taskListSelect');
+const addListButton = document.getElementById('addList');
+const listasTareas = {
+    default: new ListaTareas('Lista Principal')
+};
